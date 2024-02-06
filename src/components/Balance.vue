ec4d7249cc3dc87cbb1b96d1ec2d5adfd41e2bd6
Cheryl Lopez <clopez51@stu.southtexascollege.edu>
2024-02-06 01:06:53 -0600

    Added Balance

---
+++ src/components/Balance.vue
@@ -0,0 +1,17 @@
+<template>
+    <h4>Your Balance</h4>
+    <h1 id="balance"> {{ total }}</h1>
+</template>
+
+<script setup>
+ 
+import { DefineProps } from 'vue';
+
+const props = defineProps({
+    total: {
+        type: Number, 
+        require: true,
+    }
+})
+
+</script>
