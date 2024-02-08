960493bdf2eceb70b37d1be1e8ec7b866edf06d3
Cheryl Lopez <clopez51@stu.southtexascollege.edu>
2024-02-08 01:07:11 -0600

    generate ID

--- src/App.vue
+++ src/App.vue
@@ -41,9 +41,15 @@ const expense = computed( () => {
 //handle transaction submitted
 const handleTransactionSubmitted = (tranactionData) => {
   transactions.value.push({
+    id: generateUniqueId(),
     text: transactionData.text,
     amount: transactionData.amount,
   })
+}
+
+//generate unique id
+const generateUniqueId = () => {
+  return Math.floor(Math.random() * 10000000)
 }
 
 </script>
